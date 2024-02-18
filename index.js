82491df71b2f9c832fe91805017083127c2a4f66
JiliJiang <jiangjili@gmail.com>
2024-02-18 14:49:15 -0800

    starter code for homework 9

---
+++ index.js
@@ -0,0 +1,38 @@
+﻿const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer.prompt(
+
+[
+{type:'input',name:'title',message:'What is your project title?'},
+{type:'input',name:'description',message:'What is your project description?'},
+{type:'input',name:'installation',message:'how to install?'},
+{type:'input',name:'usage',message:'how to use?'},
+{type:'list', name:'license',message:'What is your project license?', choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']},
+
+
+//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+]
+
+
+).then((answers) => {console.log(answers);
+
+let data = createReadMe(answers);
+fs.writeFileSync(`${answers.title}.md`, data)}
+
+)
+
+
+function createReadMe(answers) {
+    return`
+# ${answers.title}
+## Description
+${answers.description}
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+${answers.license}
+    `
+}
